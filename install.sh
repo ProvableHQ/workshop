fb4653f77c776f6fc5d65918a540b19ee95c75b5
User <9260812+howardwu@users.noreply.github.com>
2022-09-17 16:15:14 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+
+if [[ $OSTYPE == 'darwin'* ]]; then
+
+# Install macOS Command Line Tools
+
+xcode-select --install > /dev/null 2>&1
+if [ 0 == $? ]; then
+    sleep 1
+    osascript <<EOD
+tell application "System Events"
+    tell process "Install Command Line Developer Tools"
+        keystroke return
+        click button "Agree" of window "License Agreement"
+    end tell
+end tell
+EOD
+else
+    echo "Command Line Developer Tools are already installed!"
+fi
+
+fi
