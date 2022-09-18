b3c9bf603d870d7afe8d42ff5716f95760963230
User <9260812+howardwu@users.noreply.github.com>
2022-09-18 23:09:48 +0300

    Update script

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 if [[ $OSTYPE == 'darwin'* ]]; then
 
 # Install macOS Command Line Tools
-
 xcode-select --install > /dev/null 2>&1
 if [ 0 == $? ]; then
     sleep 1
@@ -19,3 +18,15 @@ else
 fi
 
 fi
+
+# Clone the 'aleo' repository, or pull if it already exists.
+git clone https://github.com/AleoHQ/aleo.git aleo 2> /dev/null || (cd aleo ; git pull)
+
+# Clone the 'leo' repository, or pull if it already exists.
+git clone https://github.com/AleoHQ/leo.git leo 2> /dev/null || (cd leo ; git pull)
+
+# Install 'aleo'.
+cd aleo && cargo install --path . && cd ..
+
+# Install 'leo'.
+cd leo && cargo install --path . && cd ..
